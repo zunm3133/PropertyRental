448c4d162bc21ff4d9615b07f932c43f1558abf1
Zun Myat Hsu <moemoegyi1234567890@gmail.com>
2026-03-13 01:13:09 +0800

    modifying code

--- api/app.js
+++ api/app.js
@@ -15,7 +15,7 @@ import feedbackRoute from "./routes/feedback.route.js";
 const app = express();
 
 app.use(cors({ 
-  origin: process.env.CLIENT_URL || "http://localhost:5173", 
+  origin: process.env.CLIENT_URL, 
   credentials: true 
 }));
 app.use(express.json());
